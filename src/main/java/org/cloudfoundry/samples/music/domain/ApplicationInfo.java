f93e67bb1f7d490d9fecd48d4651973851c87ee7
Thomas Risberg <trisberg@pivotal.io>
2017-02-24 10:29:01 -0500

    Adding DNS caching info to /appinfo page

--- src/main/java/org/cloudfoundry/samples/music/domain/ApplicationInfo.java
+++ src/main/java/org/cloudfoundry/samples/music/domain/ApplicationInfo.java
@@ -3,10 +3,18 @@ package org.cloudfoundry.samples.music.domain;
 public class ApplicationInfo {
     private String[] profiles;
     private String[] services;
+    private String[] props;
 
     public ApplicationInfo(String[] profiles, String[] services) {
         this.profiles = profiles;
         this.services = services;
+        this.props = new String[0];
+    }
+
+    public ApplicationInfo(String[] profiles, String[] services, String[] props) {
+        this.profiles = profiles;
+        this.services = services;
+        this.props = props;
     }
 
     public String[] getProfiles() {
@@ -23,5 +31,13 @@ public class ApplicationInfo {
 
     public void setServices(String[] services) {
         this.services = services;
+    }
+
+    public String[] getProps() {
+        return props;
+    }
+
+    public void setProps(String[] props) {
+        this.props = props;
     }
 }
--- src/main/java/org/cloudfoundry/samples/music/web/InfoController.java
+++ src/main/java/org/cloudfoundry/samples/music/web/InfoController.java
@@ -5,11 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.Cloud;
 import org.springframework.cloud.service.ServiceInfo;
 import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+
+import sun.net.InetAddressCachePolicy;
+import sun.net.spi.nameservice.dns.DNSNameService;
 
 @RestController
 public class InfoController {
@@ -19,13 +27,62 @@ public class InfoController {
     private Environment springEnvironment;
 
     @Autowired
+    private DataSource dataSource;
+
+    @Autowired
     public InfoController(Environment springEnvironment) {
         this.springEnvironment = springEnvironment;
     }
 
     @RequestMapping(value = "/appinfo")
     public ApplicationInfo info() {
-        return new ApplicationInfo(springEnvironment.getActiveProfiles(), getServiceNames());
+        String jdbcUrl = springEnvironment.getProperty("spring.datasource.url");
+        if (jdbcUrl == null) {
+            jdbcUrl = dataSource.getUrl();
+        }
+        String jdbcHost = "unknown";
+        String jdbcIp = "?";
+        if (StringUtils.hasText(jdbcUrl)) {
+            jdbcHost = jdbcUrl.substring(jdbcUrl.indexOf("//") + 2);
+            if (jdbcHost.contains(":")) {
+                jdbcHost = jdbcHost.substring(0, jdbcHost.indexOf(":"));
+            }
+            else {
+                if (jdbcHost.contains("/")) {
+                    jdbcHost = jdbcHost.substring(0, jdbcHost.indexOf("/"));
+                } else {
+                    if (jdbcHost.contains("?")) {
+                        jdbcHost = jdbcHost.substring(0, jdbcHost.indexOf("?"));
+                    }
+                }
+            }
+            try {
+                DNSNameService ns = new DNSNameService();
+                InetAddress address = InetAddress.getByName(jdbcHost);
+                jdbcIp = address.getHostAddress();
+            } catch (UnknownHostException e) {
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        String sunCacheTtl = System.getProperty("sun.net.inetaddr.ttl");
+        String sunCacheNegTtl = System.getProperty("sun.net.inetaddr.negative.ttl");
+        String dnsCacheTtl = java.security.Security.getProperty("networkaddress.cache.ttl");
+        String dnsCacheNegTtl = java.security.Security.getProperty("networkaddress.cache.negative.ttl");
+        String cachePolicy = "sun.net.InetAddressCachePolicy#cachePolicy: " + InetAddressCachePolicy.get();
+        String cacheNegPolicy = "sun.net.InetAddressCachePolicy#negativeCachePolicy: " + InetAddressCachePolicy.getNegative();
+        return new ApplicationInfo(springEnvironment.getActiveProfiles(), getServiceNames(),
+                new String[] {
+                        "Config:javax.sql.Datasource#class: " + dataSource.getClass().getName(),
+                        "Config:javax.sql.Datasource#url: " + dataSource.getUrl(),
+                        "Config:spring.datasource.url: " + jdbcUrl,
+                        "Config:mysql.host: " + jdbcHost + " : " + jdbcIp,
+                        "System:sun.net.inetaddr.ttl: " + sunCacheTtl,
+                        "System:sun.net.inetaddr.negative.ttl: " + sunCacheNegTtl,
+                        "Security:networkaddress.cache.ttl: " + dnsCacheTtl,
+                        "Security:networkaddress.cache.negative.ttl: " + dnsCacheNegTtl,
+                        cachePolicy,
+                        cacheNegPolicy});
     }
 
     @RequestMapping(value = "/service")
